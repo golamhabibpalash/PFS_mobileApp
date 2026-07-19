375b9642829ec28604db166190f8d021c4d1f631
golamhabibpalash <golamhabibpalash@gmail.com>
2026-07-19 17:48:05 +0600

    fix: update Xcode Cloud CI script to use Bundler for CocoaPods

--- ci_scripts/ci_pre_xcodebuild.sh
+++ ci_scripts/ci_pre_xcodebuild.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-# Install CocoaPods before Xcode Cloud build
-cd "$CI_WORKSPACE/ios"
-pod install
+# Install CocoaPods via Bundler before Xcode Cloud build
+cd "$CI_WORKSPACE"
+
+if ! command -v bundle &> /dev/null; then
+    gem install bundler --no-document
+fi
+
+bundle install
+cd ios
+bundle exec pod install
