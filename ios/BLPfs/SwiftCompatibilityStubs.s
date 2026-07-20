cab39c083653163fad9fa314a67ba23883ed8f3b
golamhabibpalash <golamhabibpalash@gmail.com>
2026-07-20 15:51:54 +0600

    fix: use assembly stubs for Swift compatibility symbols (correct Mach-O symbol names)

---
+++ ios/BLPfs/SwiftCompatibilityStubs.s
@@ -0,0 +1,13 @@
+.text
+.globl _SwiftCompatibilityStub
+_SwiftCompatibilityStub:
+  .long 0
+
+.globl __swift_FORCE_LOAD_$_swiftCompatibilityConcurrency
+__swift_FORCE_LOAD_$_swiftCompatibilityConcurrency:
+  .long 0
+
+.globl __swift_FORCE_LOAD_$_swiftCompatibility56
+__swift_FORCE_LOAD_$_swiftCompatibility56:
+  .long 0
+
